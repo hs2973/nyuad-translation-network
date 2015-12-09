5c6006a19da52692d7f8d89439e90044725ccf44
thecodeater <himal.shrestha@nyu.edu>
2015-12-09 19:21:41 +0400

    New repository created

--- php/application/views/admin/welcome.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-	<div class="jumbotron">
-	  <h1>Hello Admin!</h1>
-	  <p>Welcome to Translation Network.</p>
-	  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-	</div>
-</div>
--- php/application/views/includes/template.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$this->load->model('articles_model');
-	$data['new_submissions_count'] = $this->articles_model->count_articles(array('is_approved' => '0'));
-?>
-<?php $this->load->view('includes/header', $data); ?>
-
-<?php $this->load->view($main_content); ?>
-
-<?php $this->load->view('includes/footer'); ?>
